7c68e36465075b1e345af7077b033b73db54900d
AsisASotelo <asis.a.sotelo@gmail.com>
2021-03-28 20:04:07 -0700

    Begin Deqeue Files

---
+++ Week2/queues/Deque.java
@@ -0,0 +1,44 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+
+    // Construct an empty Deque
+    public Deque() {
+        // Create using Nodes
+    }
+
+    // is the deque empty?
+    public boolean isEmpty()
+
+    // return the number of items on the deque
+    public int size()
+
+    // add the item to the front
+    public void addFirst(Item item)
+
+    // add the item to the back
+    public void addLast(Item item)
+
+    // remove and return the item from the front
+    public Item removeFirst()
+
+    // remove and return the item from the back
+    public Item removeLast()
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator()
+
+    // unit testing (required)
+    public static void main(String[] args)
+
+
+    public static void main(String[] args) {
+
+    }
+}
